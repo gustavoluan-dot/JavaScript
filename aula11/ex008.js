9735b1afc76c0a9b1764a518d4cb4636b873660a
gustavoluan-dot <gu.luan232@gmail.com>
2025-07-03 09:24:54 -0300

    att with "Condições"

---
+++ aula11/ex008.js
@@ -0,0 +1,13 @@
+var  vel = 60.5
+console.log(`Voce esta a ${vel} Km/h`)
+if (vel > 60) { //Condicao simples
+    console.log('MULTADO!')
+}
+console.log('Dirija sempre com segurança!')
+
+var city = 'RJ'
+if (city =='SP') {
+    console.log('Olá, paulista!')
+} else {
+    console.log('Olá, gringo!')
+}
---
+++ aula11/ex009.js
@@ -0,0 +1,21 @@
+var country = 'Brasil'
+console.log(`Voce vive no ${country}!`)
+if (country != 'Canadá') {
+    console.log('Você é Canadense!')
+} else {
+    console.log('Você é Estrangeiro!')
+}
+var idade = 17
+console.log(`Olá, voce tem ${idade}`)
+if (idade >= 18) {
+    console.log('Voce pode ir')
+} else {
+    console.log('Voce nao poder ir')
+}
+
+var time = 'Palmeiras'
+if (time == 'Corinthians') {
+    console.log('Seja bem vindo ao mundo de Loucos!')
+} else {
+    console.log('Sai daqui porco imundo')
+}
