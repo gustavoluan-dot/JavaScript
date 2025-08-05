3d73ef0d2f5ec54cdf76d93c321d31dc2a6124af
gustavoluan-dot <gu.luan232@gmail.com>
2025-08-05 20:40:25 -0300

    more atts

---
+++ aula15/ambiente.js
@@ -0,0 +1,9 @@
+
+let num = [5, 8, 2, 3]
+num.push(1)
+num.push(7)
+
+console.log(`Esse é o número dos vetores ${num}`)
+console.log(`O vetor em ordem crescente fica assim: ${num.sort()}`)
+console.log(`O vetor tem ${num.length} posições`)
+
