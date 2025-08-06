5459d09fad4fdc752ea0f64acf272b5eaf8b282f
gustavoluan-dot <gu.luan232@gmail.com>
2025-08-06 19:41:23 -0300

    more studies

---
+++ aula16/funcao01.js
@@ -0,0 +1,9 @@
+function imppar (n) {
+    if (n%2==0) {
+        return 'Par!'
+    } else {
+        return 'Ímpar!'
+    } 
+}
+
+console.log(imppar(50))
---
+++ aula16/funcao04.js
@@ -0,0 +1,10 @@
+function fatorias(n) {
+    let fat = 1
+    for (let c = n; c > 1; c--) {
+        fat *= c
+    }
+
+    return fat 
+}
+
+console.log(fatorias(5))
